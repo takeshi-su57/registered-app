74e9eefed55a8e60a75586a2157e877ad220d25d
hard-working-diamondcreator <lucas.lee.work61@gmail.com>
2022-05-15 11:55:14 -0300

    Feat: cria base de frontend com next ts

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,10 @@
+import '../styles/globals.css';
+import '../styles/Home.css';
+
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp;
