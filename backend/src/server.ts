bc30eb1cfafed3dede01c130563a759c5b0d2dcf
hard-working-diamondcreator <lucas.lee.work61@gmail.com>
2022-05-15 18:56:01 -0300

    Fix: deploy frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = '3001';
 
+app.get('/', (req, res) => {
+  res.status(200).send('<h1>Servidor Online</h1>');
+});
+
 app.listen(PORT, () => {
   console.log(`👉 Online server on port ${PORT}!`);
 });
