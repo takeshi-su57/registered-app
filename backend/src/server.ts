4dc2d6145da56d14c2ad233b2d0c39d04e919df4
hard-working-diamondcreator <lucas.lee.work61@gmail.com>
2022-05-15 22:47:10 -0300

    Fix: dockerfile

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import usersRouter from './routes/usersRouter';
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
