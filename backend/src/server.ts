4809968b37ba3b57fa4fcf1c9a4ed3db9dcfb480
hard-working-diamondcreator <lucas.lee.work61@gmail.com>
2022-05-18 22:45:27 -0300

    Feat(backend): adiciona cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,14 @@
 import * as express from 'express';
+import * as cors from 'cors';
 import errorMiddleware from './middlewares/error';
 import usersRouter from './routes/usersRouter';
-
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/users', usersRouter);
 
