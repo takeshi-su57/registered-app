355c8d3ef3b0ccae9642ab58929a59d89a9be076
hard-working-diamondcreator <lucas.lee.work61@gmail.com>
2022-05-25 15:56:15 -0300

    Feat(Frontend): adiciona botao de adicionar novo user - versao mobile

--- frontend/components/ControlPanel.tsx
+++ frontend/components/ControlPanel.tsx
@@ -37,6 +37,12 @@ const ControlPanel: NextComponentType = () => {
       <button className={ styles.btnNewUser } onClick={ () => Router.push('/create') }>
         Registrar novo usuário
       </button>
+
+      <div className={ styles.containerBtnMobile }>
+        <button className={ styles.btnNewUserMobile } onClick={ () => Router.push('/create') }>
+          <svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"></path></svg>
+        </button>
+      </div>
     </div>
   )
 }
